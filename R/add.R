4d49f4123a8a0feb7966c178c3dbf9571ad5c084
yiqiao zhu <sgyzhu@umich.edu>
2024-11-17 21:49:35 -0500

    241117

---
+++ R/add.R
@@ -0,0 +1,15 @@
+
+#' @title Add Two Number
+#' @description XXXXXXXXXX
+#'     YYYYYYYYYYYY
+#'
+#' @param x one number
+#' @param y one number
+#'
+#' @return sum of two numbers
+#' @export
+#'
+#' @examples add(1,2)
+add <- function(x,y){
+  x + y
+}
